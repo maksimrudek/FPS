fbcd3dd70653fd5463ac83c07470df2cf98d7a43
Maksim Rudek <maksim.rudek@gmail.com>
2023-05-10 17:43:02 +0300

    finish

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -7,42 +7,46 @@ public class Gun : MonoBehaviour
     public GameObject hitPrefab;
     public GameObject muzzlePrefab;
     public float maxDistance = 100;
-    public float gunfireTime;
-    public AudioSource gunshotSound;
+
+    public AudioClip gunshotSound;
+    private AudioSource source;
 
     private void Start()
     {
-
-        muzzlePrefab.SetActive(false);
+        source = gameObject.AddComponent<AudioSource>();
     }
 
     private void Update()
     {
-        var ray = new Ray(transform.position, transform.forward);
-
-        if (Time.time < gunfireTime)
-        {
-            muzzlePrefab.SetActive(true);
-        }
-        else
-        {
-            muzzlePrefab.SetActive(false);
-        }
-
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
-            muzzlePrefab.SetActive(true);
-
-            if (Physics.Raycast(ray,out var hit, maxDistance))
-            {
-                print(hit.point);
-                var hitObj = Instantiate(hitPrefab, hit.point, Quaternion.Euler(0, 0, 0));
-                hitObj.transform.forward = hit.normal;
-                hitObj.transform.position += hit.normal * 0.02f;
-                gunfireTime = Time.time + 0.1f;
-                gunshotSound.Play();
-
-            }
+            Shoot();
         }
+    }
+
+    void Shoot()
+    {
+        var cam = Camera.main;
+        var ray = new Ray(cam.transform.position, cam.transform.forward);
+
+        muzzlePrefab.SetActive(true);
+        Invoke("DisableFlashEffect", 0.05f);
+
+        source.pitch = Random.Range(0.8f, 1.2f);
+        source.PlayOneShot(gunshotSound);
+
+        if (Physics.Raycast(ray, out var hit, maxDistance))
+        {
+             print(hit.point);
+             var hitObj = Instantiate(hitPrefab, hit.point, Quaternion.Euler(0, 0, 0));
+             hitObj.transform.forward = hit.normal;
+             hitObj.transform.position += hit.normal * 0.02f;
+
+        }
+    }
+
+    void DisableFlashEffect()
+    {
+        muzzlePrefab.SetActive(false);
     }
 }
